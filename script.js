4c4d002686597e2eff434bfb3639331525785d49
Vlad <105812180+VladProkopov@users.noreply.github.com>
2022-05-28 21:58:22 +0300

    dz3 pause

---
+++ script.js
@@ -0,0 +1,74 @@
+//Задание 1
+for(let a = 1; a <=50; a++) {
+    console.log(a);
+}
+
+for(let b = 35; b >=8; b--) {
+    console.log(b);
+}
+
+// Задание 2
+let c = 89;
+
+while(c >= 11) {
+    console.log(c);
+    c--;
+}
+
+// Задание 3
+let sum = 0;
+
+for(let i = 1; i <= 100; i++) {
+    sum +=i;
+
+    console.log('sum ' + i + ' = ' + sum);
+}
+
+// Задание 4
+for (let i = 1; i <= 5; i++) {
+    sum = 0;
+    for (let j = 1; j <= i; j++) {
+        sum += j;
+    }
+
+    console.log('sum ' + i + ' = ' + sum);
+}
+
+// Задание 5
+for(let i = 8; i <= 56; i++) {
+    if (i % 2 == 0)
+        console.log(i);
+}
+
+i = 8;
+while(i <=56 ) {
+    if (i % 2 == 0)
+    console.log(i);
+    i++;
+}
+
+// Задание 6
+for(let i = 2; i < 10; i++) {
+    sum = 0;
+    for(let j = 1; j < 10; j++){
+        sum = i * j;
+    console.log(i + ' * ' + j + ' = ' + sum);
+    }
+}
+
+// Задание 7
+let n = 1000;
+    Number = 0;
+    while (n > 50) {
+        n = n /2;
+        Number++;
+    }
+
+console.log(Number);
+
+
+// Задание 8
+
+
+// Задание 9
+// Задание 10
